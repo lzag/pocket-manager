ae5d4416c68a16179877d8ca1717cc552dc42aef
Lukasz Zagroba <lukasz.zagroba@adsmurai.com>
2020-03-05 16:24:15 +0100

    Removes the consumer key from the init file

--- init.php
+++ init.php
@@ -1,7 +1,5 @@
 <?php
 
-const CONSUMER_KEY = '80613-c75e5b2f359c163253b130ff';
-
 spl_autoload_register(function ($class) {
     $class_name = preg_replace('/App\\\/', '/src/', $class);
     include __DIR__ . $class_name . '.class.php';
